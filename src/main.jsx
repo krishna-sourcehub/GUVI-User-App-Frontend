564fee44639379beea4b34bd36840f63b43c4135
krishna-sourcehub <vkrishnan569@gmail.com>
2024-01-29 02:19:04 +0000

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+    import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="https://auth-back-uilb.onrender.com">
-    <App />
-    </BrowserRouter>
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
 
-);
+    );
